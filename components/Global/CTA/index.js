8ac2a2e1fd3bf161dc7c7d76aefdb13f9aadd394
VovaVindar <volodimir.budkevych@gmail.com>
2025-01-27 00:25:39 -0800

    Added states to CTA

--- components/Global/CTA/index.js
+++ components/Global/CTA/index.js
@@ -1,8 +1,11 @@
 import Scrollbar from "@/components/Global/Scrollbar";
 import { useContact } from "@/context/ScrollbarContext";
+import { useRouter } from "next/router";
 
-export default function ContactScrollbar({ className }) {
+export default function CTA({ className }) {
   const { contactOpen, setContactOpen } = useContact();
+  const router = useRouter();
+  const isHomePage = router.pathname === "/";
 
   const handleClick = async () => {
     setContactOpen(false);
@@ -10,11 +13,39 @@ export default function ContactScrollbar({ className }) {
     startLenis();
   };
 
+  // Determine the props based on state
+  const getScrollbarProps = () => {
+    if (contactOpen) {
+      // State 2: Close contact popover
+      return {
+        text: "Close",
+        onClick: handleClick,
+        href: undefined,
+      };
+    } else if (!isHomePage) {
+      // State 3: On any page except home, show "Close" that links to home
+      return {
+        text: "Close",
+        onClick: undefined,
+        href: "/",
+      };
+    } else {
+      // State 1: On homepage, open contact popover
+      return {
+        text: "Contact",
+        onClick: undefined,
+        href: "mailto:vovavindar@gmail.com",
+      };
+    }
+  };
+
+  const props = getScrollbarProps();
+
   return (
     <Scrollbar
-      text={contactOpen ? "Close" : "Contact"}
-      href={contactOpen ? undefined : "mailto:vovavindar@gmail.com"}
-      onClick={contactOpen ? handleClick : undefined}
+      text={props.text}
+      href={props.href}
+      onClick={props.onClick}
       className={className}
     />
   );
